147bd7573f788a858dab29ed5668c853d0964478
chaturs <chaturs@miamioh.edu>
2025-02-22 10:40:00 -0500

    feat: add umbrella header

---
+++ include/repl/repl.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+/** @file repl.hpp
+ *  @brief Umbrella header for the REPL calculator.
+ */
+
+#include "repl/evaluator.hpp"
+#include "repl/expression.hpp"
+#include "repl/state.hpp"
+#include "repl/token.hpp"
+#include "repl/errors.hpp"
